a14cedfa0cd764cb79541fb449e27a048bd1df62
kevinLL22 <lopez.kevin.45678@gmail.com>
2023-05-15 19:38:39 -0500

    initialze dependencies

---
+++ src/main/java/co/kevinl/forumapirestful/ForumApiRestfulApplication.java
@@ -0,0 +1,13 @@
+package co.kevinl.forumapirestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumApiRestfulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForumApiRestfulApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/kevinl/forumapirestful/ForumApiRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kevinl.forumapirestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApiRestfulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
