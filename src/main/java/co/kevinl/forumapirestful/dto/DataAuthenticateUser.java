37d053c0956f605cd0a568dcb15ff78131c90cef
kevinLL22 <lopez.kevin.45678@gmail.com>
2023-05-17 19:12:02 -0500

    dto DataAuthenticateUser created and method to return a Authentication manager

---
+++ src/main/java/co/kevinl/forumapirestful/dto/DataAuthenticateUser.java
@@ -0,0 +1,8 @@
+package co.kevinl.forumapirestful.dto;
+
+public record DataAuthenticateUser(
+
+        String email,
+        String password
+) {
+}
--- src/main/java/co/kevinl/forumapirestful/service/security/SecurityConfigurations.java
+++ src/main/java/co/kevinl/forumapirestful/service/security/SecurityConfigurations.java
@@ -1,6 +1,9 @@
 package co.kevinl.forumapirestful.service.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -10,11 +13,16 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfigurations {
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
 
         return security.csrf().disable()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().build();
+    }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
